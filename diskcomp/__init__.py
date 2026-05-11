d34a85bca4402f04daadc7af05e8a67a64ded78e
christian_os <83128748+christianstenbro@users.noreply.github.com>
2026-05-11 21:26:00 +0200

    Updated trial list path to new set

--- diskcomp/__init__.py
+++ diskcomp/__init__.py
@@ -38,7 +38,7 @@ class C(BaseConstants):
     PLAYERS_PER_GROUP: int | None = None
     NUM_ROUNDS: int = 1
     STIM_PATH: Path = Path("stimuli")
-    STIM_CSV: Path = Path(__file__).parent / "_private/trial_list_session_2.csv" # updated by CS, 30-04-2026
+    STIM_CSV: Path = Path(__file__).parent / "_private/trial_list_session_3.csv" # updated by CS, 11-05-2026
     STIM_IMAGE_CSV: Path = Path(__file__).parent / "_private/stim.csv"
     NUM_PRACTICE_TRIALS: int = 3
     TRIALS_IN_BLOCK: int = 107 # updated from 107
